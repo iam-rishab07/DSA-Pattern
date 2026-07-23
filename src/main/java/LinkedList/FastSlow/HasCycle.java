9ac36d7e0af4942e72b75392c27b5c064474bbd5
iam-rishab07 <vrushabgorivale42740@gmail.com>
2026-07-23 18:34:56 +0530

    linked-list

---
+++ src/main/java/LinkedList/FastSlow/HasCycle.java
@@ -0,0 +1,22 @@
+package LinkedList.FastSlow;
+
+import LinkedList.ListNode;
+
+// leetcode 141 : linked list cycle
+public class HasCycle {
+    public boolean hasCycle(ListNode head) {
+        if(head==null || head.next==null) return false;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next!=null && fast.next.next!=null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow==fast)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
